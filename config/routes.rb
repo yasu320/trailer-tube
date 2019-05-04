7f84fd9b4570a69ed6aab126f191099a7e44adb0
yasu320 <yasu320.musashi@gmail.com>
2019-05-04 19:10:10 +0900

    Add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [:show]
   root to: 'videos#index'
 end
