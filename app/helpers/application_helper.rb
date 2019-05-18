5726db85d05a05643624e75eb8b94e4f919d5f97
yasu320 <yasu320.musashi@gmail.com>
2019-05-18 11:32:38 +0900

    #9 Add page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Trailer Tube"
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title}・#{base_title}"
+    end
+  end
 end
