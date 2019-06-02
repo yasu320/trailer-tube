f48ccdb09f903b6bad6678f8552312fd3e25b38c
yasu320 <yasu320.musashi@gmail.com>
2019-06-02 14:54:29 +0900

    Introduce factory bot

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :development, :test do
   gem 'pry-rails'
   gem 'bullet'
   gem 'rspec-rails', '~> 3.6.0'
+  gem "factory_bot_rails", "~> 4.10.0"
 end
 
 group :development do
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    username "testuser"
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password "test123"
+    description "description"
+    birth_date "1979-10-15"
+    sex "男性"
+    website "https://test.com/"
+  end
+end
