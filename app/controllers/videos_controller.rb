9b68e86096cd3585133ceb6dbc1c9266d156d335
yasu320 <yasu320.musashi@gmail.com>
2019-06-14 22:57:45 +0900

    #32 Introduce ransack and make search bar

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,17 @@
 class VideosController < ApplicationController
   MAX_RESULTS = 3
   def index
-    @videos = Video.page(params[:page]).per(18).recent
+    @q = Video.ransack(params[:q])
+    @videos = @q.result.page(params[:page]).per(18).recent
   end
 
   def show
     @video = Video.find(params[:id])
     @reviews = @video.reviews.includes(user: { image_attachment: :blob }).limit(MAX_RESULTS).recent
+  end
+
+  def search
+    index
+    render :index
   end
 end
